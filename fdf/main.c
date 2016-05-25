4f03df68937d1b161da6c1719fe372c6edabdf7c
Johan MEIRING <jomeirin@e5r1p6.jnb.42.fr>
2016-05-25 14:34:42 +0200

    FDF and Filler

---
+++ fdf/main.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jomeirin <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/05/25 14:26:31 by jomeirin          #+#    #+#             */
+/*   Updated: 2016/05/25 14:28:50 by jomeirin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+Remember to exploit your libft! The use of get_next_line, ft_split and ft_getnbr will allow you to quickly and simply read the data from the file.
+Regarding the graphic representation:
+• You can choose the type of projection: parallel, iso, conic.
+• You must manage expose correctly.
+• You must be able to quit the program by pressing ’esc’.
+• The use of images from minilibX is strongly encouraged.
+• You will find in the subject’s attachements files related to the topic (a binary called fdf as well as the example 42.fdf inside fdf.zip).
+*/
+
+int main(int arg, char **args)
+{
+
+}
