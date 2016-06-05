3c464b855c6b2c74d312f958e32b9f9c43de2bea
myne <emjmeiring@gmail.com>
2016-06-05 03:34:04 +0200

    Hexa and octal added

--- backup/ft_printf/ft_printf.h
+++ backup/ft_printf/ft_printf.h
@@ -24,68 +24,46 @@ typedef char 			t_bool;
 typedef	unsigned int	pos_int;
 typedef	size_t			t_size;
 
-typedef struct			s_format
+typedef	struct		s_flags
 {
-	const char			*string;
-	t_size				parse_loc;
-	t_size				parse_pass;	
-}						t_format;
+	t_bool			left_justified;
+	t_bool			zero_padding;
+	t_bool			sign;
+	t_bool			blanks;
+	t_bool			hashtag;
+}					t_flags;
 
-//static void				input_lexer(t_format *format);
-//static void				input_parser(t_format *format, va_list args);
-
-typedef	struct			s_flags
+typedef	struct		s_precision
 {
-	t_bool				left_justified;
-	t_bool				zero_padding;
-	t_bool				sign;
-	t_bool				blanks;
-	t_bool				hashtag;
-}						t_flags;
+	unsigned int	precision;
+	unsigned int	width;
+	t_bool			period;
+}					t_pc;
 
-//static void				flag_lexer(t_format *format);
-
-typedef	struct			s_precision
-{
-	unsigned int		precision;
-	unsigned int		width;
-	t_bool				period;
-}						t_pc;
-
-//static void				precison_lexer(t_format *format);
-
-typedef enum			e_len_mod
+typedef enum	e_len_mod
 {
 	HH, H, L, LL, J, Z, NOT_SPECIFIED
-}						t_length;
+}				t_length;
 
-typedef enum			e_spec
+typedef enum	e_spec
 {
 	S_DECIMAL, OCTAL, HEX_UPPER, U_DECIMAL, CHAR, STRING, POINTER,
 	INVALID_SPEC, HEX_LOWER
-}						t_spec;
+}				t_spec;
 
-typedef	struct 			s_convertor
+typedef	struct	s_convertor
 {
-	t_flags				flags;
-	t_pc				precision;
-	t_length			length;
-	t_bool				format_spec;
-//	t_specifier			specifier;
-}						t_convertor;
+	t_flags		flags;
+	t_pc		precision;
+	t_length	length;
+	t_bool		format_spec;
+}				t_convertor;
 
-char					*print_able_digit(t_convertor conv, char *str_arg);
-char					*print_able_string(t_convertor conv, char *str_arg);
-void					master_chef(t_convertor conv, va_list args);
-int						ft_printf(const char *format, ...);
-int						get_flag(t_flags *comv_flag, char flag);
-int						get_width(t_pc *precision,const char *stream, int prec);
-
-//int						specifier_lexer(const char *runner, va_list args);
-//static void				conversion_lexer(t_format *format);
-//static void				input_lexer(t_format *format, va_list args);
-//static void				parse(t_format *format, va_list args);
-//static void				syntax_error(char *party_pooper, t_format *format);
-//char 						*hexa_converter(t_convertor conv, va_list args);
+int				ft_printf(const char *format, ...);
+char			*print_able_digit(t_convertor conv, char *str_arg, int *count);
+char			*print_able_string(t_convertor conv, char *str_arg, int *count);
+int				master_chef(t_convertor conv, va_list args);
+int				get_flag(t_flags *comv_flag, char flag);
+int				get_width(t_pc *precision,const char *stream, int prec);
 
 #endif
